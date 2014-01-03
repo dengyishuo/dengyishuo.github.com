a39cf0d3a60a2242372f411f3f0fbddc620dc710
dengyishuo <dengyishuo@163.com>
2014-01-03 20:52:19 +0800

    new blog

--- Btcoin Investment/Rcode/backtest_plot.r
+++
@@ -1,5 +0,0 @@
-# Visualize the performance of a investment strategy or a portfolio.
-backtest_plot <- function(){
-
-
-}
--- Btcoin Investment/Rcode/price_trend_signal.r
+++
@@ -1,7 +0,0 @@
-price_trend_signal <- function(btdata,n=20){
-	btdata$sma <- SMA(btdata$price,n);
-	btdata$diff <- btdata$price-btdata$sma
-	flag <- function(x){ifelse(x>=0,1,-1)}
-	btdata$signal <- apply(btdata$signal,1,flag);
-	btdata <- as.data.frame(na.omit(btdata))
-}
